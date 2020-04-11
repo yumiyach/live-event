69e98f37db85f564abf38b935daa7a73989dc208
DESKTOP-V140RQE\yumiy <yumiyachizuru@gmail.com>
2020-04-11 17:38:02 +0900

    ブース作成

--- pages/index.vue
+++ pages/index.vue
@@ -22,12 +22,48 @@
         </v-layout>
       </v-carousel-item>
     </v-carousel>
-    <v-container>
+    <v-container style="margin-bottom:112px">
       <v-content>
-        <p class="display-3">{{ boothList[model].name }}</p>
+        <v-row class="mx-0 my-4 align-center">
+          <div class="display-1">{{ boothList[model].name }}</div>
+          <div class="ml-auto">
+            <v-btn class="mx-1" fab dark small color="#55acee">
+              <v-icon dark>mdi-twitter</v-icon>
+            </v-btn>
+            <v-btn fab dark small>
+              <v-icon dark>mdi-gift</v-icon>
+            </v-btn>
+          </div>
+        </v-row>
+
+        <v-row>
+          <v-col v-for="(item, i) in boothList[model].itemList" :key="i" cols="12" sm="6" lg="4">
+            <v-card>
+              <v-row class="ma-0">
+                <v-col class="pa-0">
+                  <v-img style="height:100%" :src="item.image" aspect-ratio="0.707"/>
+                </v-col>
+                <v-col class="pa-0" style="display: flex;flex-direction: column;">
+                  <v-card-title v-text="item.name"></v-card-title>
+                  <v-card-subtitle v-text="item.description"></v-card-subtitle>
+                  <v-card-actions class="mt-auto">
+                    <v-btn
+                      v-for="(link, k) in item.linkList"
+                      :key="k"
+                      :href="link.url"
+                      target="_blank"
+                      small
+                      outlined
+                    >{{link.text}}</v-btn>
+                  </v-card-actions>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-content>
     </v-container>
-    <v-footer fixed>
+    <v-footer fixed class="pa-0" color="#ffffff88">
       <v-content>
         <v-layout justify-center>
           <v-slide-group v-model="model" class="pa-4" mandatory center-active>
@@ -44,8 +80,9 @@
                     justify="center"
                     :style="{
                     backgroundColor: '#000',
-                    opacity: active ? 0 : 0.6
-                  }"
+                    opacity: active ? 0 : 0.6,
+                    color:'#fff',
+                    }"
                   >{{ booth.name }}</v-row>
                 </v-img>
               </v-card>
@@ -71,20 +108,81 @@ export default {
       slides: ['First', 'Second', 'Third', 'Fourth', 'Fifth'],
       boothList: [
         {
-          name: 'サークル名1',
-          image: require('~/assets/images/background.jpg')
+          name: 'こぶんやさん',
+          image: 'https://maroasobi.com/wp-content/uploads/hyoen1.jpg',
+          itemList: [
+            {
+              name: 'とさにっき！',
+              image:
+                'https://images-na.ssl-images-amazon.com/images/I/51N8UiIo3EL._SX366_BO1,204,203,200_.jpg',
+              description:
+                '紀貫之女体化ドタバタギャグ。土佐旅行へ赴任した貫之が京都へ帰る途中、事件に巻き込まれます。',
+              linkList: [
+                { text: '通販', url: 'https://www.amazon.co.jp/dp/4043574207' },
+                {
+                  text: 'サンプル',
+                  url:
+                    'https://www.aozora.gr.jp/cards/000155/files/832_16016.html'
+                }
+              ]
+            },
+            {
+              name: '源氏物語',
+              image: 'https://m.media-amazon.com/images/I/61w4fOd-X3L.jpg',
+              description:
+                '光源氏×紫の上イチャラブNL。光源氏に過去の女がいたような描写があります注意。',
+              linkList: [
+                {
+                  text: '青空文庫',
+                  url:
+                    'https://www.aozora.gr.jp/cards/000052/files/5016_9758.html'
+                }
+              ]
+            },
+            {
+              name: 'とさにっき！',
+              image:
+                'https://images-na.ssl-images-amazon.com/images/I/51N8UiIo3EL._SX366_BO1,204,203,200_.jpg',
+              description:
+                '紀貫之女体化ドタバタギャグ。土佐旅行へ赴任した貫之が京都へ帰る途中、事件に巻き込まれます。',
+              linkList: [
+                { text: '通販', url: 'https://www.amazon.co.jp/dp/4043574207' },
+                {
+                  text: 'サンプル',
+                  url:
+                    'https://www.aozora.gr.jp/cards/000155/files/832_16016.html'
+                }
+              ]
+            },
+            {
+              name: '源氏物語',
+              image: 'https://m.media-amazon.com/images/I/61w4fOd-X3L.jpg',
+              description:
+                '光源氏×紫の上イチャラブNL。光源氏に過去の女がいたような描写があります注意。',
+              linkList: [
+                {
+                  text: '青空文庫',
+                  url:
+                    'https://www.aozora.gr.jp/cards/000052/files/5016_9758.html'
+                }
+              ]
+            }
+          ]
         },
         {
           name: 'サークル名2',
-          image: require('~/assets/images/background.jpg')
+          image: require('~/assets/images/background.jpg'),
+          itemList: []
         },
         {
           name: 'サークル名3',
-          image: require('~/assets/images/background.jpg')
+          image: require('~/assets/images/background.jpg'),
+          itemList: []
         },
         {
           name: 'サークル名4',
-          image: require('~/assets/images/background.jpg')
+          image: require('~/assets/images/background.jpg'),
+          itemList: []
         }
       ],
       model: 0
