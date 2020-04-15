260380cf3505c37dbfaf2d24a6416dd2e59c62dc
yumiya_chizuru <yumiya.chizuru@members.co.jp>
2020-04-15 09:08:56 +0900

    ライブ配信タブと在席・離席フラグを作成

--- pages/booth.vue
+++ pages/booth.vue
@@ -4,8 +4,9 @@
       <v-spacer></v-spacer>
       <v-toolbar-title
         class="font-weight-black"
-        :style="{color:$vuetify.theme.currentTheme.primary}"
-      >オンラインイベント</v-toolbar-title>
+        :style="{ color: $vuetify.theme.currentTheme.primary }"
+        >オンラインイベント</v-toolbar-title
+      >
       <v-spacer></v-spacer>
       <v-btn text small color="primary" to="/">TOP</v-btn>
     </v-app-bar>
@@ -26,16 +27,21 @@
               height: 'calc(100vh - 220px)'
             }"
           >
-            <v-img :src="booth.image" style="height:100%"/>
+            <v-img :src="booth.image" style="height:100%" />
           </v-layout>
         </v-layout>
       </v-carousel-item>
     </v-carousel>
     <v-container style="margin-bottom:112px">
       <v-content>
-        <v-row class="mx-0 my-4 align-center">
-          <div class="display-1">{{ boothList[model].name }}</div>
-          <div class="ml-auto">
+        <v-row class="mx-0 mt-4 align-center">
+          <v-layout class="display-1 mb-4" align-center >
+            <v-chip class="ma-2" color="primary">
+              在席中
+            </v-chip>
+            {{ boothList[model].name }}
+          </v-layout>
+          <div class="ml-auto mb-4">
             <v-btn class="mx-1" fab dark small color="#55acee">
               <v-icon dark>mdi-twitter</v-icon>
             </v-btn>
@@ -48,6 +54,12 @@
         <v-tabs v-model="tab" grow>
           <v-tab class="title">頒布物</v-tab>
           <v-tab class="title">コメント</v-tab>
+          <v-tab class="title">
+            ライブ
+            <v-avatar class="ml-2" size="20">
+              <v-icon small>mdi-antenna</v-icon>
+            </v-avatar>
+          </v-tab>
         </v-tabs>
 
         <v-tabs-items v-model="tab">
@@ -63,11 +75,20 @@
                 <v-card>
                   <v-row class="ma-0">
                     <v-col class="pa-0">
-                      <v-img style="height:100%" :src="item.image" aspect-ratio="0.707"/>
+                      <v-img
+                        style="height:100%"
+                        :src="item.image"
+                        aspect-ratio="0.707"
+                      />
                     </v-col>
-                    <v-col class="pa-0" style="display: flex;flex-direction: column;">
+                    <v-col
+                      class="pa-0"
+                      style="display: flex;flex-direction: column;"
+                    >
                       <v-card-title v-text="item.name"></v-card-title>
-                      <v-card-subtitle v-text="item.description"></v-card-subtitle>
+                      <v-card-subtitle
+                        v-text="item.description"
+                      ></v-card-subtitle>
                       <v-card-actions class="mt-auto">
                         <v-btn
                           v-for="(link, k) in item.linkList"
@@ -76,7 +97,8 @@
                           target="_blank"
                           small
                           outlined
-                        >{{link.text}}</v-btn>
+                          >{{ link.text }}</v-btn
+                        >
                       </v-card-actions>
                     </v-col>
                   </v-row>
@@ -90,24 +112,87 @@
               <v-list-item>
                 <v-list-item-content>
                   <v-text-field label="名前"></v-text-field>
-                  <v-textarea name="input-7-1" label="メッセージ" rows="3"></v-textarea>
+                  <v-textarea
+                    name="input-7-1"
+                    label="メッセージ"
+                    rows="3"
+                  ></v-textarea>
                   <v-btn depressed small color="primary">投稿</v-btn>
                 </v-list-item-content>
               </v-list-item>
-              <v-divider/>
+              <v-divider />
               <div v-for="(item, i) in chatList[model]" :key="i">
                 <v-list-item>
                   <v-list-item-content>
-                    <p class="mb-2">{{item.message}}</p>
+                    <p class="mb-2">{{ item.message }}</p>
                     <v-list-item-subtitle class="d-flex">
-                      <div class="mr-2">{{item.name}}</div>
-                      <div
-                        class="shrink ml-auto"
-                      >{{item.date.getFullYear()}}/{{(item.date.getMonth()+1)}}/{{(item.date.getDate())}} {{item.date.getHours()}}:{{(item.date.getMinutes())}}:{{(item.date.getSeconds())}}</div>
+                      <div class="mr-2">{{ item.name }}</div>
+                      <div class="shrink ml-auto">
+                        {{ item.date.getFullYear() }}/{{
+                          item.date.getMonth() + 1
+                        }}/{{ item.date.getDate() }}
+                        {{ item.date.getHours() }}:{{
+                          item.date.getMinutes()
+                        }}:{{ item.date.getSeconds() }}
+                      </div>
                     </v-list-item-subtitle>
                   </v-list-item-content>
                 </v-list-item>
-                <v-divider/>
+                <v-divider />
+              </div>
+              <v-list-item>
+                <v-list-item-content>
+                  <p class="mb-2">メッセージは以上です</p>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-tab-item>
+
+          <v-tab-item>
+            <v-list three-line>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>配信中！</v-list-item-title>
+                </v-list-item-content>
+
+                <v-spacer></v-spacer>
+
+                <v-list-item-icon>
+                  <v-btn icon>
+                    <v-icon>mdi-play</v-icon>
+                  </v-btn>
+                </v-list-item-icon>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-text-field label="名前"></v-text-field>
+                  <v-textarea
+                    name="input-7-1"
+                    label="メッセージ"
+                    rows="3"
+                  ></v-textarea>
+                  <v-btn depressed small color="primary">投稿</v-btn>
+                </v-list-item-content>
+              </v-list-item>
+              <v-divider />
+              <div v-for="(item, i) in chatList[model]" :key="i">
+                <v-list-item>
+                  <v-list-item-content>
+                    <p class="mb-2">{{ item.message }}</p>
+                    <v-list-item-subtitle class="d-flex">
+                      <div class="mr-2">{{ item.name }}</div>
+                      <div class="shrink ml-auto">
+                        {{ item.date.getFullYear() }}/{{
+                          item.date.getMonth() + 1
+                        }}/{{ item.date.getDate() }}
+                        {{ item.date.getHours() }}:{{
+                          item.date.getMinutes()
+                        }}:{{ item.date.getSeconds() }}
+                      </div>
+                    </v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-divider />
               </div>
               <v-list-item>
                 <v-list-item-content>
@@ -136,9 +221,9 @@
                     align="center"
                     justify="center"
                     :style="{
-                    backgroundColor: '#000',
-                    opacity: active ? 0 : 0.6,
-                    color:'#fff',
+                      backgroundColor: '#000',
+                      opacity: active ? 0 : 0.6,
+                      color: '#fff'
                     }"
                   >
                     <v-col class="text-center">{{ booth.name }}</v-col>
