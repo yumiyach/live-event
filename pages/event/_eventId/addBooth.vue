affd4369d13b7d387695ce2d1fd37c57e319f064
DESKTOP-V140RQE\yumiy <yumiyachizuru@gmail.com>
2020-05-02 11:03:14 +0900

    頒布物の画像選択

--- pages/event/_eventId/addBooth.vue
+++ pages/event/_eventId/addBooth.vue
@@ -8,40 +8,60 @@
         max-width="876"
       >
         <v-card-title class="display-1 accent--text">
-          <v-icon large left color="accent">mdi-account</v-icon>『{{
-            eventData.name
+          <v-icon large left color="accent">mdi-account</v-icon>
+          『{{
+          eventData.name
           }}』に出展する
         </v-card-title>
         <v-card-text>
           <v-form ref="form" lazy-validation @submit.prevent>
-            <v-file-input :rules="imageRules" @change="onFilePicked" accept="image/*" label="ヘッダー画像"></v-file-input>
+            <v-file-input
+              :rules="imageRules"
+              @change="onFilePicked"
+              accept="image/*"
+              label="ヘッダー画像"
+            ></v-file-input>
             <h2>頒布物一覧</h2>
             <v-row>
-              <v-col
-                v-for="(item, i) in itemList"
-                :key="i"
-                cols="12"
-                sm="6"
-                lg="4"
-              >
+              <v-col v-for="(item, i) in itemList" :key="i" cols="12" sm="6">
                 <v-card>
                   <v-row class="ma-0">
                     <v-col class="pa-0">
                       <v-img
-                        style="height:100%"
-                        :src="item.image"
+                        :src="item.imageUrl? item.imageUrl : require('~/assets/images/no_image.png')"
                         aspect-ratio="0.707"
-                      />
+                        @click="onItemImageClicked('itemImage_'+i)"
+                      ></v-img>
+                      <input
+                        :index="i"
+                        :ref="'itemImage_'+i"
+                        :id="'itemImage_'+i"
+                        :name="'itemImage_'+i"
+                        type="file"
+                        accept="image/*"
+                        @change="onItemImagePicked"
+                        style="display:none"
+                      >
                     </v-col>
-                    <v-col
-                      class="pa-0"
-                      style="display: flex;flex-direction: column;"
-                    >
-                      <v-card-title v-text="item.name"></v-card-title>
-                      <v-card-subtitle
-                        v-text="item.description"
-                      ></v-card-subtitle>
+                    <v-col class="pa-0" style="display: flex;flex-direction: column;">
+                      <v-card-title>
+                        <v-text-field
+                          v-model="itemList[i].name"
+                          :counter="50"
+                          class="mb-3"
+                          label="タイトル"
+                        ></v-text-field>
+                      </v-card-title>
+                      <v-card-subtitle>
+                        <v-textarea
+                          v-model="itemList[i].desctiption"
+                          class="body-2"
+                          label="説明文"
+                          :counter="500"
+                        ></v-textarea>
+                      </v-card-subtitle>
                       <v-card-actions class="mt-auto">
+                        <v-btn small>リンク追加</v-btn>
                         <v-btn
                           v-for="(link, k) in item.linkList"
                           :key="k"
@@ -49,8 +69,7 @@
                           target="_blank"
                           small
                           outlined
-                          >{{ link.text }}</v-btn
-                        >
+                        >{{ link.text }}</v-btn>
                       </v-card-actions>
                     </v-col>
                   </v-row>
@@ -69,13 +88,7 @@
           outlined
         ></v-text-field>
         <v-card-actions class="pa-4 mx-auto" style="max-width:600px">
-          <v-btn
-            x-large
-            style="width:100%"
-            color="accent"
-            @click="submit"
-            :loading="isLoading"
-          >
+          <v-btn x-large style="width:100%" color="accent" @click="submit" :loading="isLoading">
             <v-icon left>mdi-check</v-icon>出展する
           </v-btn>
         </v-card-actions>
@@ -97,7 +110,15 @@ export default {
   data: () => ({
     isLoading: false,
     headerImage: null,
-    itemList: [],
+    itemList: [
+      {
+        name: '',
+        description: '',
+        imageUrl: null,
+        imageFile: false,
+        urlList: []
+      }
+    ],
     wishListUrl: null
   }),
   computed: {
@@ -127,6 +148,24 @@ export default {
       })
 
       this.getEvent(this.eventId)
+    },
+    onItemImageClicked(ref) {
+      this.$refs[ref][0].click()
+    },
+    onItemImagePicked(e) {
+      const index = parseInt(e.target.attributes[0].value)
+      const file = e.target.files[0]
+      if (file !== undefined) {
+        const fr = new FileReader()
+        fr.addEventListener('load', () => {
+          this.itemList[index].imageUrl = fr.result
+          this.itemList[index].imageFile = file
+        })
+        fr.readAsDataURL(file)
+      } else {
+        this.itemList[index].imageUrl = null
+        this.itemList[index].imageFile = false
+      }
     },
     onFilePicked(e) {
       const file = e
