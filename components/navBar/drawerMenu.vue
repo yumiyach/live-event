c808c51f4379e421360ed30ce3619c082cf71480
DESKTOP-V140RQE\yumiy <yumiyachizuru@gmail.com>
2020-05-23 17:23:54 +0900

    こまごま

--- components/navBar/drawerMenu.vue
+++ components/navBar/drawerMenu.vue
@@ -5,11 +5,11 @@
         <v-btn block color="primary" @click="$refs.loginDialog.visible = true">
           <v-icon left>mdi-login</v-icon>ログイン
         </v-btn>
-        <loginDialog ref="loginDialog" />
+        <loginDialog ref="loginDialog"/>
       </v-list-item>
       <v-list-item v-else>
         <v-list-item-avatar>
-          <v-img :src="loginUser.photoURL" />
+          <v-img :src="loginUser.photoURL"/>
         </v-list-item-avatar>
         <v-list-item-content>
           <v-list-item-title>{{ loginUser.displayName }}</v-list-item-title>
@@ -18,41 +18,42 @@
     </v-list>
 
     <v-list>
-      <v-divider />
+      <v-divider/>
       <v-list-item @click="onPressCreateEvent">
         <v-list-item-title>イベント作成</v-list-item-title>
       </v-list-item>
-      <v-divider />
+      <v-divider/>
+      <v-list-item href="https://trello.com/b/PdniZKBA/" target="_blank">
+        <v-list-item-title>不具合と更新予定の一覧表</v-list-item-title>
+      </v-list-item>
+      <v-divider/>
       <v-list-item
         href="https://docs.google.com/forms/d/e/1FAIpQLSdnW0_HoPtNdZJrk-QBvawTE_yGD_5MI_ENmZnQOJF3fSjSnA/viewform"
         target="_blank"
       >
         <v-list-item-title>お問い合わせ</v-list-item-title>
       </v-list-item>
-      <v-divider />
+      <v-divider/>
       <v-list-item nuxt to="/">
         <v-list-item-title>トップページ</v-list-item-title>
       </v-list-item>
-      <v-divider />
-      <v-list-item
-        href="https://marshmallow-qa.com/matoi_yumiya"
-        target="_blank"
-      >
+      <v-divider/>
+      <v-list-item href="https://marshmallow-qa.com/matoi_yumiya" target="_blank">
         <v-list-item-title>マシュマロ</v-list-item-title>
       </v-list-item>
-      <v-divider />
+      <v-divider/>
     </v-list>
 
     <v-list v-if="isLogin">
-      <v-divider />
+      <v-divider/>
       <v-list-item @click="$refs.logoutDialog.visible = true">
         <v-list-item-action>
           <v-icon left>mdi-logout</v-icon>
         </v-list-item-action>
         <v-list-item-title>ログアウト</v-list-item-title>
       </v-list-item>
-      <v-divider />
-      <logoutDialog ref="logoutDialog" />
+      <v-divider/>
+      <logoutDialog ref="logoutDialog"/>
     </v-list>
   </div>
 </template>
